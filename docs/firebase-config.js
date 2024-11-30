bf8789e2186973068d051bc2ae2a4a86c8c25901
soliverson <stacy.oliverson@gmail.com>
2024-11-30 12:08:25 -0700

    update firebase-config

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
@@ -17,4 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export default db;
+export { db };
