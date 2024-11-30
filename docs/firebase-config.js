0812a064ca832ee2d2c2b730c8f1dfb3c18d1465
soliverson <stacy.oliverson@gmail.com>
2024-11-30 12:51:38 -0700

    fixed firebase

--- docs/firebase-config.js
+++ docs/firebase-config.js
@@ -1,17 +1,20 @@
+// Import the functions you need from the Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyAVW6kkkQ8xn8x3cYe_BmvBa78v_qeW1H8",
+  authDomain: "event-planner-b35e7.firebaseapp.com",
+  projectId: "event-planner-b35e7",
+  storageBucket: "event-planner-b35e7.appspot.com",
+  messagingSenderId: "940381941062",
+  appId: "1:940381941062:web:ca6cbd964b23e5ba614947",
+  measurementId: "G-1Y9JWMCT5P",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db as default }; // Explicitly export `db` as the default export
+export default db;
